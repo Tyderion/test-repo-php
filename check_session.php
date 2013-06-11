ba21782fd3d4882cfd44c2627528a2fd2f4399cf
Tyderion <nadlergabriel@gmail.com>
2013-06-11 09:44:58 +0200

    test

--- check_session.php
+++ check_session.php
@@ -1,8 +1,9 @@
 <?php
 
-session_start()
-if (isset($_SESSION["logged_in"])) {
-	header("Location: login.php");
-	die();
-}
+// session_start();
+// if (!isset($_SESSION["logged_in"])) {
+// 	header("Location: login.php");
+// 	die();
+// }
+
 ?>
